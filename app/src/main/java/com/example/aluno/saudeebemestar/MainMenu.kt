8b2045caae6ed1cc6b1ac3bd742e301836754818
AmadeoNeto <amadeotatocota@gmail.com>
2019-05-27 10:11:24 -0300

    Mudanças nas activities e arquivo README

--- app/src/main/java/com/example/aluno/saudeebemestar/MainMenu.kt
+++ app/src/main/java/com/example/aluno/saudeebemestar/MainMenu.kt
@@ -8,5 +8,7 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
     }
 }
