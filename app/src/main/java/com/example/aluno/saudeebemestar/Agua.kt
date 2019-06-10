9f3992a67ad2baeb60c85e1068b31d4dd22557d4
Aluno <aluno@naverecife.local>
2019-06-10 09:56:42 -0300

    Finalização nas activities agua, alimentacao, desafios. Finalização do  codigo da agua

--- app/src/main/java/com/example/aluno/saudeebemestar/Agua.kt
+++ app/src/main/java/com/example/aluno/saudeebemestar/Agua.kt
@@ -2,11 +2,40 @@ package com.example.aluno.saudeebemestar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_agua.*
 
 class Agua : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agua)
+
+        var value: Int = 0;
+        val button150 = findViewById<Button>(R.id.button150)
+        val button200 = findViewById<Button>(R.id.button200)
+        val button500 = findViewById<Button>(R.id.button500)
+        val progress = findViewById<ProgressBar>(R.id.waterBar)
+        val text = findViewById<TextView>(R.id.waterText)
+
+        button150.setOnClickListener {
+            progress.progress += 150
+            value+= 150
+            waterText.text = value.toString() + "ml/2000ml"
+        }
+
+        button200.setOnClickListener {
+            progress.progress += 200
+            value += 200
+            waterText.text = value.toString() + "ml/2000ml"
+        }
+
+        button500.setOnClickListener {
+            progress.progress += 500
+            value += 500
+            waterText.text = value.toString() + "ml/2000ml"
+        }
     }
 }
