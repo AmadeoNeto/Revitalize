7661ff953b88c67a4ab2e8fc624011cdcff59835
AmadeoNeto <amadeotatocota@gmail.com>
2019-08-17 22:46:58 -0300

    Adjusts in the desafios_layout.xml

--- app/src/main/java/com/example/aluno/saudeebemestar/DesafiosRV.kt
+++ app/src/main/java/com/example/aluno/saudeebemestar/DesafiosRV.kt
@@ -17,10 +17,26 @@ class DesafiosRV : AppCompatActivity() {
         setContentView(R.layout.activity_desafios_rv)
 
         var desafioList = mutableListOf<Desafio>()
+
+        desafioList.let{
+            it.add(Desafio("Diminua o uso de açúcar refinado",
+                    "Dica: use o mascavo, ele é bem mais saudável",R.drawable.sugar,false))
+            it.add(Desafio("Faça 30 abdominais todos os dias",
+                    "Essa é uma órima opção para fortalecer a musculatura do abdômen", R.drawable.abdominal, false))
+            it.add(Desafio("Diminua o consumo de enlatados",
+                    "O nível elevado de sódio desses alimentos pode causar problemas de saúde",R.drawable.caned_food,false))
+            it.add((Desafio("Diminua a quanridade de lipídios",
+                    "A presença exacerbada deles no nosso  aumenta a chance do entupimento de veias", R.drawable.fast_food, false)))
+        }
+
         viewManager = LinearLayoutManager(this)
         adapter = DesafioAdapter(desafioList)
 
         desafioRV.adapter = adapter
         desafioRV.layoutManager = viewManager
     }
+    //abdominal - Photo by bruce mars from Pexels
+    //sugar - Photo by Suzy Hazelwood from Pexels
+    //cannedFood - Photo by edwin josé vega ramos from Pexels
+    //fastFood - Photo by rawpixel.com from Pexels
 }
